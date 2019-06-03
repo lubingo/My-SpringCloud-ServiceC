5b6abc7fcca4e3190b1fcd9be9f1b9b6f7057c06
lubing <15810172080@139.com>
2019-06-03 09:58:36 +0800

    负载

--- src/main/java/com/cloud/springcloud/redis/util/RedisUtils.java
+++ src/main/java/com/cloud/springcloud/redis/util/RedisUtils.java
@@ -5,7 +5,9 @@ import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -13,10 +15,11 @@ import java.util.concurrent.TimeUnit;
 public class RedisUtils {
 
     @Autowired
-    private RedisTemplate redisTemplate ;
+    private RedisTemplate redisTemplate;
 
     /**
      * 删除对应的value
+     *
      * @param key
      */
     public void remove(final String key) {
@@ -24,8 +27,10 @@ public class RedisUtils {
             redisTemplate.delete(key);
         }
     }
+
     /**
      * 批量删除对应的value
+     *
      * @param keys
      */
     public void remove(final String... keys) {
@@ -37,6 +42,7 @@ public class RedisUtils {
 
     /**
      * 判断缓存中是否有对应的value
+     *
      * @param key
      * @return
      */
@@ -110,98 +116,115 @@ public class RedisUtils {
     }
 
 
-
     /**
      * 哈希 添加
+     *
      * @param key
      * @param hashKey
      * @param value
      */
-    public void hmSet(String key, Object hashKey, Object value){
+    public void hmSet(String key, Object hashKey, Object value) {
         HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
-        hash.put(key,hashKey,value);
+        hash.put(key, hashKey, value);
     }
 
     /**
      * 哈希获取数据
+     *
      * @param key
      * @param hashKey
      * @return
      */
-    public Object hmGet(String key, Object hashKey){
-        HashOperations<String, Object, Object>  hash = redisTemplate.opsForHash();
-        return hash.get(key,hashKey);
+    public Object hmGet(String key, Object hashKey) {
+        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+        return hash.get(key, hashKey);
     }
-
 
 
     /**
      * 列表添加
+     *
      * @param k
      * @param v
      */
-    public void lPush(String k,Object v){
+    public void lPush(String k, Object v) {
         ListOperations<String, Object> list = redisTemplate.opsForList();
-        list.rightPush(k,v);
+        list.rightPush(k, v);
     }
 
     /**
      * 列表获取
+     *
      * @param k
      * @param l
      * @param l1
      * @return
      */
-    public List<Object> lRange(String k, long l, long l1){
+    public List<Object> lRange(String k, long l, long l1) {
         ListOperations<String, Object> list = redisTemplate.opsForList();
-        return list.range(k,l,l1);
+        return list.range(k, l, l1);
     }
-
 
 
     /**
      * 集合添加
+     *
      * @param key
      * @param value
      */
-    public void add(String key,Object value){
+    public void add(String key, Object value) {
         SetOperations<String, Object> set = redisTemplate.opsForSet();
-        set.add(key,value);
+        set.add(key, value);
     }
 
     /**
      * 集合获取
+     *
      * @param key
      * @return
      */
-    public Set<Object> setMembers(String key){
+    public Set<Object> setMembers(String key) {
         SetOperations<String, Object> set = redisTemplate.opsForSet();
         return set.members(key);
     }
 
 
-
     /**
      * 有序集合添加
+     *
      * @param key
      * @param value
      * @param scoure
      */
-    public void zAdd(String key,Object value,double scoure){
+    public void zAdd(String key, Object value, double scoure) {
         ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        zset.add(key,value,scoure);
+        zset.add(key, value, scoure);
     }
 
     /**
      * 有序集合获取
+     *
      * @param key
      * @param scoure
      * @param scoure1
      * @return
      */
-    public Set<Object> rangeByScore(String key,double scoure,double scoure1){
+    public Set<Object> rangeByScore(String key, double scoure, double scoure1) {
         ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
         return zset.rangeByScore(key, scoure, scoure1);
     }
 
+    public  Set<String> gtAllKey() {
+        return redisTemplate.keys("*");
+
+    }
+
+    public String getKeyValue(Set <String> set ){
+        Map<String ,String>  map  =  new HashMap<String ,String>() ;
+        for(String key  : set){
+            String value = redisTemplate.opsForValue().get(key).toString();
+            map.put(key,value);
+        }
+        return map.toString() ;
+    }
 }
